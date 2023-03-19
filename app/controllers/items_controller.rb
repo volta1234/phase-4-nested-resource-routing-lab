a73727487dd16b5937ca338ed75b349872b8e39d
CALVINCE <calvince.ogwel@gmail.com>
2023-03-19 23:59:06 +0300

    completed nested resource routing lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
-
   def index
-    items = Item.all
-    render json: items, include: :user
+    user = User.find_by(id: params[:user_id])
+    if user
+      items = user.items
+      render json: items.as_json(include: :user)
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
   end
 
+  def show
+    @item = User.find(params[:user_id]).items.find_by(id: params[:id])
+    if @item
+      render json: @item
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.build(item_params)
+
+    if item.save
+      render json: item, status: :created
+    else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show, :create]
+  end
+
   resources :items, only: [:index]
-  resources :users, only: [:show]
 end
